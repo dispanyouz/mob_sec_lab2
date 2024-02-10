339c60189d93eeed2a93fa050682644d14eea812
magaking17 <yeltaev17@gmail.com>
2024-02-10 22:29:54 +0600

    sd

---
+++ app/src/main/java/com/example/sis2125temirlan_kurmanbek/activity_second_screen.kt
@@ -0,0 +1,11 @@
+package com.example.sis2125temirlan_kurmanbek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_second_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_screen)
+    }
+}
